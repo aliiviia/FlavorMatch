3cd9ad38d93af59c95789ee4c1de8e88d44558ba
Alivia DiPrimeo <alivia.diprimeo@gmail.com>
2025-11-04 20:16:30 -0500

    Created a cuisine to genre map

---
+++ backend/cuisineGenreMap.js
@@ -0,0 +1,11 @@
+export default {
+  mexican: "salsa",
+  italian: "classical",
+  japanese: "lofi",
+  indian: "bollywood",
+  american: "country",
+  french: "jazz",
+  chinese: "pop",
+  thai: "dance",
+  greek: "folk",
+};
